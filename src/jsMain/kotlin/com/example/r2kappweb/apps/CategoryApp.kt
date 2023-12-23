ccfb9fb43bef131868fabfac543c43f8b3433b0d
CarlosAlfaroP <144950990+CarlosAlfaroP@users.noreply.github.com>
2023-12-22 22:03:40 -0300

    Cards amodificadas y flexPanel centrado

--- src/jsMain/kotlin/com/example/r2kappweb/apps/CategoryApp.kt
+++ src/jsMain/kotlin/com/example/r2kappweb/apps/CategoryApp.kt
@@ -2,10 +2,14 @@ package com.example.r2kappweb.apps
 
 import io.kvision.core.*
 import io.kvision.html.div
+import io.kvision.html.image
+import io.kvision.html.link
 import io.kvision.panel.FieldsetPanel
 import io.kvision.panel.fieldsetPanel
 import io.kvision.panel.flexPanel
+import io.kvision.utils.perc
 import io.kvision.utils.px
+import io.kvision.utils.vh
 
 class CategoryApp : DesktopWindow("Temario", "fa-solid fa-book-open", 900, 400) {
     init {
@@ -17,24 +21,25 @@ class CategoryApp : DesktopWindow("Temario", "fa-solid fa-book-open", 900, 400) 
         height = 450.px
         width = 600.px
         padding = 2.px
-        // Usar un flexPanel para organizar las Cards
+
         flexPanel(FlexDirection.ROW, FlexWrap.WRAP, alignContent = AlignContent.STRETCH, alignItems = AlignItems.STRETCH) {
-            // Añadir Cards (usando fieldsetPanel) para cada tema
+            justifyContent = JustifyContent.CENTER
             for (i in 1..7) {
-                add(createThemeCard("Tema $i", "Descripción del Tema $i"))
+                add(createCard("Tema $i", "Descripción del Tema $i", "Una descripcion de $i"))
             }
         }
     }
-
-    private fun createThemeCard(title: String, description: String): FieldsetPanel {
-        return fieldsetPanel(title) {
+    private fun createCard(titulo: String, subtitulo: String, descripcion: String): FieldsetPanel {
+        return fieldsetPanel(titulo) {
             paddingTop = 10.px
             paddingBottom = 10.px
-            div(description)
-            // Puedes agregar aquí más elementos a la tarjeta, como botones o imágenes
+            margin = 10.px
+            div(subtitulo){ addCssClass("card-subtitle")}
+            image(src = "static/banner.png", alt = "").apply { width = 100.perc }
+            div(descripcion) { addCssClass("card-text")}
+            link("Ir a...", "#")
         }
     }
-
     companion object {
         fun run(container: Container) {
             container.add(CategoryApp())
